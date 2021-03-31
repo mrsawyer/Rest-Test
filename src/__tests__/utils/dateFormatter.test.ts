a3f9ae8a2d4908ad541759d5a29845d6f55bdc72
Montana Sawyer <montanarsawyer@gmail.com>
2021-03-31 15:48:23 -0700

    BenchRT - update test

--- src/__tests__/utils/dateFormatter.test.ts
+++ src/__tests__/utils/dateFormatter.test.ts
@@ -1,6 +1,6 @@
 import { dateFormatter } from "../../utils/dateFormatter";
 
-describe("currencyFormatter", () => {
+describe("dateFormatter", () => {
   test("Formats date string into correct format", () => {
     expect(dateFormatter("2013-12-22")).toBe("Dec 22, 2013");
   })
